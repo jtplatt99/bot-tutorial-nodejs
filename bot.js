e2364b9e587fd077e3c141f45e1ee0ab1971211d
jtplatt99 <jonathantplatt@gmail.com>
2020-05-05 10:59:11 -0400

    Try to make webpage more useful
    Make webpage more useful, and add logic specific to one user.

--- bot.js
+++ bot.js
@@ -17,9 +17,10 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]);
   
   // Regular expressions matching
-  var CaseOne = /^\d+$/;		// Case 1: Just a number from a user
-  var CaseTwo = /^\/TB max$/;
-  var CaseThree = /^\/TB links$/;
+  var CaseOne = /^\d+$/;			// Case 1: Just a number from a user
+  var CaseTwo = /^\/tb max$/;		// Case 2: Request maximum prices
+  var CaseThree = /^\/tb links$/;	// Case 3: Request links for prices
+  var CaseFour = /^d+ K$/;			// Case 4: Enter for Kim Kendra
 
   // If we input a new value
   if(request.text && CaseOne.test(request.text)) {
@@ -69,8 +70,8 @@ function respond() {
 		}
 	  }
   
-  	  var response = highestGuaranteedMinimum[0] + ' has the highest guaranteed minimum of ' + highestGuaranteedMinimum[1] + '\n' +
-			         highestMaximum[0] + ' has the highest overall maximum of ' + highestMaximum[1];
+  	  var response = highestGuaranteedMinimum[0] + ' has the highest guaranteed minimum of ' + highestGuaranteedMinimum[1] + '.\n' +
+			         highestMaximum[0] + ' has the highest overall maximum of ' + highestMaximum[1] + '.';
 	  // Actually send the message back to groupme
 	  this.res.writeHead(200);
       postMessage(response);
@@ -98,6 +99,34 @@ function respond() {
       postMessage(response);
       this.res.end();
 	});
+
+	// If we enter a price for Kim Kendra
+  } else if(request.text && CaseFour.test(request.text)) {
+	// This is where we add our additional logic
+	//console.log(JSON.stringify(request));	
+	var time = moment(request.created_at).tz("UTC").tz("America/New_York");
+	var dayOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+	var day = dayOfWeek[time.day()];
+	if(day != 'Sunday') {
+	  if(time.hour() < 12) {
+		day += 'AM';
+	  } else {
+		day += 'PM';
+	  }
+	}
+
+	pool.query('SELECT UserName FROM TurnipPrices WHERE UserName=\'Kim Kendra\';', (err, sqlres) => {
+	  console.log(sqlres.rows);
+	  if(!!sqlres.rows.length) { // Execute if this user exists
+	    pool.query('UPDATE TurnipPrices SET ' + day + '=' + request.text + 'WHERE UserName=\'Kim Kendra\';', (err, sqlres2) => {});
+	  } else {				// Create new row if user doesn't exist
+	    pool.query('INSERT INTO TurnipPrices (UserID, UserName, ' + day + ') \
+	      VALUES (934, \'Kim Kendra\', ' + request.text + ');', (err, sqlres2) => {});
+	  }
+	});
+
+	this.res.writeHead(200);
+    this.res.end();
 
   // Otherwise we don't need to respond
   } else {
@@ -196,4 +225,38 @@ function deSQL(row) {
   return prices;
 }
 
+function tabulater() {
+  var response = '<!doctype html><html><body><table border="1"><thead><tr><th rowspan="2">User</th><th rowspan="2">Sunday</th><th colspan="2">Monday</th><th colspan="2">Tuesday</th><th colspan="2">Wednesday</th><th colspan="2">Thursday</th><th colspan="2">Friday</th><th colspan="2">Saturday</th><th rowspan="2">Guaranteed Min</th><th rowspan="2">Week Max</th></tr><tr><th>AM</th><th>PM</th><th>AM</th><th>PM</th><th>AM</th><th>PM</th><th>AM</th><th>PM</th><th>AM</th><th>PM</th><th>AM</th><th>PM</th></tr></thead><tbody>';
+
+  pool.query('SELECT * FROM TurnipPrices;', (err, sqlres) => {
+    if(err) {
+	  console.log(err);
+	  console.log(sqlres);
+	  console.log(process.env.DATABASE_URL);
+    }
+    for(let row of sqlres.rows) {
+	  response += '<tr>';
+	  
+	  var prices = deSQL(row);
+	  var prediction = new Predictor(prices,false);
+	  var possibilities = (prediction.analyze_possibilities())[0];
+	  
+	  response += '<th><a href="https://turnipprophet.io/?prices=' + prices.slice(1).join('.') + '">' +
+	              row.username + '</a></th>';
+	  for(i = 1; i < 14; i++) {
+		if(prices[i] != null) {
+		  response += '<th>' + prices[i] + '</th>';
+		}
+		else {
+		  response += '<th>' + possibilities.prices[i].min + '..' + possibilities.prices[i].max + '</th>';
+		}
+	    response += '<th>' + possibilities.weekGuaranteedMinimum + '</th><th>' + possibilities.weekMax + '</th>';
+	  }
+	  response += '</tr>';
+  }
+  response += '</tbody></table></body></html>';
+  return response;
+}
+
 exports.respond = respond;
+exports.tabulator = tabulator;
